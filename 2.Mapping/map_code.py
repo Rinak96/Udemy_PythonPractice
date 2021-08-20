491d39b9c873fca00663de5e10c60b06d62d9224
Rinak96 <rina.k@campus.technion.ac.il>
2021-08-20 19:57:59 +0300

    corona map success

--- 2.Mapping/map_code.py
+++ 2.Mapping/map_code.py
@@ -1,7 +1,38 @@
 import folium
+import pandas as pd
+import geopy
+from geopy.geocoders import ArcGIS
+import json
+
+def color_producer(color_in_hebrew):
+    if color_in_hebrew == "אדום":
+        return 'red'
+    elif color_in_hebrew == "ירוק":
+        return 'green'
+    elif color_in_hebrew == "צהוב":
+        return 'yellow'
+    elif color_in_hebrew == "כתום":
+        return 'orange'
+    else:
+        return 'gray'
 
 map = folium.Map(location=[32.6778,35.2417])
+df = pd.read_csv("ramzor1908.csv")
 
-map.add_child(folium.Marker(location=[32.6778,35.2417],popup="Hi this is my home town", icon=folium.Icon(color='red')))
+nom = ArcGIS()
+fg = folium.FeatureGroup(name="My Map")
 
-map.save("Map1.html")
+data = json.load(open("world.json",'r',encoding='utf-8-sig'))
+city_names = list(df["שם יישוב"])
+colors = list(df["צבע לפי חישוב יומי"])
+
+for city in city_names:
+    location = nom.geocode(city)
+    if location != None :
+        latitude = location.latitude
+        longitude = location.longitude
+        color = df.at[(df[df['שם יישוב'] == city].index)[0],'צבע לפי חישוב יומי']
+        fg.add_child(folium.CircleMarker(location=[latitude,longitude],radius = '6', fill_color = color_producer(color), fill_opacity = 0.8))
+    
+map.add_child(fg)
+map.save("Corona_map_Israel.html")
